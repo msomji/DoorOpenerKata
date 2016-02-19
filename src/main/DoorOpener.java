181b882495ae28cef9ae90b0b7f38fb739ab1d54
Maisam <maisam_somji@gap.com>
2016-02-19 14:08:25 -0500

    completed door opener logic

--- src/main/DoorOpener.java
+++ src/main/DoorOpener.java
@@ -1,24 +1,56 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DoorOpener {
 
 	public int numberOfDoors;
-	List<String> updatedArray;
+	public List<String> updatedArray;
 
 	public List<String> convert(List<String> stringArray) {
 		numberOfDoors = stringArray.size();
 		updatedArray = stringArray;
-		for (String door : updatedArray) {
-			updatedArray.set(updatedArray.indexOf(door), update(door));
-		}
+		
+		itterateThroughArray();
 		return updatedArray;
 	}
+
+	private int itterateThroughArray() {
+		int round = 1;
+		while (round <= numberOfDoors){
+			updateDoorPosition(round);
+			round++;
+		}
+		return round;
+	}
+
+	private void updateDoorPosition(int round) {
+		int position = 0 ;
+		for (String door : updatedArray) {
+			if ((position + 1) % round == 0){
+				updatedArray.set(position, update(door));
+			};
+			position++;
+		}
+	};
 
 	public String update(String status) {
 		return status == "open" ? "close" : "open";
 	}
+
+	public List<Integer> openDoorPositions() {
+		List<Integer> resultArray = new ArrayList<Integer>();
+		
+		int position = 1;
+		for (String status : updatedArray){
+			if (status == "open"){
+				resultArray.add(position);
+			}
+		position++;
+		}
+		return resultArray;
+	};
 
 }
 
--- src/test/DoorOpenerTest.java
+++ src/test/DoorOpenerTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import main.DoorOpener;
@@ -41,7 +42,7 @@ public class DoorOpenerTest {
 	
 	@Test
 	public void shouldsetLengthOfArrayTo1(){
-		stringArray.add("closed");
+		setDoors(1);
 		underTest.convert(stringArray);
 		
 		assertThat(underTest.numberOfDoors, is(1));
@@ -49,12 +50,89 @@ public class DoorOpenerTest {
 
 	@Test
 	public void shouldOpenAllDoorsIfTotalIs1(){
-		stringArray.add("close");
+		setDoors(1);
 		List<String> result = new ArrayList<String>();
 		result.add("open");
-		assertThat(underTest.convert(stringArray), is(result));
 		
+		assertThat(underTest.convert(stringArray), is(result));
 	}
+	
+	@Test
+	public void shouldHaveOneOpenAndOneCloseddoorIfTotalLengthIS2(){
+		setDoors(2);
+		
+		List<String> result = new ArrayList<String>();
+		result.add("open");
+		result.add("close");
+		
+		assertThat(underTest.convert(stringArray), is(result));
+	}
+	
+	@Test
+	public void should2ClosedDoorsAnd2OpenDoorsWithTotalOf4(){
+		setDoors(4);
+		
+		
+		List<String> result = new ArrayList<String>();
+		result.add("open");
+		result.add("close");
+		result.add("close");
+		result.add("open");
+		
+		assertThat(underTest.convert(stringArray), is(result));
+	}
+	
+	@Test
+	public void shouldReturnCorrectcombinationOfOpenClosedDoorsFor5Doors(){
+		setDoors(5);
+		
+		List<String> result = new ArrayList<String>();
+		result.add("open");
+		result.add("close");
+		result.add("close");
+		result.add("open");
+		result.add("close");
+		
+		assertThat(underTest.convert(stringArray), is(result));
+	};
+
+	@Test
+	public void shouldReturnPositionsOfOpenDoorsWith5Total(){
+		setDoors(5);
+		underTest.convert(stringArray);
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		result.add(1);
+		result.add(4);
+		assertThat(underTest.openDoorPositions(), is(result));
+	}
+	
+	@Test
+	public void shouldReturnPositionsOfOpenDoorsWith100Total(){
+		setDoors(100);
+		underTest.convert(stringArray);
+		ArrayList<Integer> result = new ArrayList<Integer>();
+
+		result.add(1);
+		result.add(4);
+		result.add(9);
+		result.add(16);
+		result.add(25);
+		result.add(36);
+		result.add(49);
+		result.add(64);
+		result.add(81);
+		result.add(100);
+		
+		assertThat(underTest.openDoorPositions(), is(result));
+	}
+	
+	public void setDoors(int number){
+		while(number > 0){
+			stringArray.add("close");
+			number--;
+		};
+	};
 }
 
 
